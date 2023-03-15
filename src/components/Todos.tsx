79e1bdea4ff4aef3852263fcf1b63cfb5438c392
Thuc Van Truong <truongthucvan1242@gmail.com>
2023-03-16 00:40:16 +0700

    feat: work with props

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,16 @@
-function Todos() {
+import React from "react";
+
+// React >= 18: https://stackoverflow.com/questions/59106742/typescript-error-property-children-does-not-exist-on-type-reactnode
+const Todos: React.FC<{ items: string[] }> = (props) => {
   return (
     <ul>
-      <li>Learn React</li>
-      <li>Learn TypeScript</li>
+      {/* <li>Learn React</li>
+      <li>Learn TypeScript</li> */}
+      {props.items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
     </ul>
   );
-}
+};
 
 export default Todos;
