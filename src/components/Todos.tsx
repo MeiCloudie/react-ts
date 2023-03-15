dc7d04079facff3cf67b4cba2e26114b0669a458
Thuc Van Truong <truongthucvan1242@gmail.com>
2023-03-16 00:51:23 +0700

    refactor: add a data model

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import Todo from '../models/todo'
 
 // React >= 18: https://stackoverflow.com/questions/59106742/typescript-error-property-children-does-not-exist-on-type-reactnode
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {/* <li>Learn React</li>
       <li>Learn TypeScript</li> */}
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
