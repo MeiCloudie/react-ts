9322aeecfa5b679e72e5c63f293240b93c8989e4
Thuc Van Truong <truongthucvan1242@gmail.com>
2023-03-16 01:58:03 +0700

    feat: remove a Todo

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Todo from '../models/todo'
+import Todo from "../models/todo";
 import TodoItem from "./TodoItem";
-import classes from './Todos.module.css';
+import classes from "./Todos.module.css";
 
 // React >= 18: https://stackoverflow.com/questions/59106742/typescript-error-property-children-does-not-exist-on-type-reactnode
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={classes.todos}>
       {/* <li>Learn React</li>
@@ -12,7 +14,11 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
 
       {props.items.map((item) => (
         // <li key={item.id}>{item.text}</li>
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
