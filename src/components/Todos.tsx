26d03371f7cbbc900ffd00360822758e37ccc3c1
Thuc Van Truong <truongthucvan1242@gmail.com>
2023-03-16 01:00:23 +0700

    feat: add a component - TodoItem.tsx

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Todo from '../models/todo'
+import TodoItem from "./ToDoItem";
 
 // React >= 18: https://stackoverflow.com/questions/59106742/typescript-error-property-children-does-not-exist-on-type-reactnode
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
@@ -7,8 +8,10 @@ const Todos: React.FC<{ items: Todo[] }> = (props) => {
     <ul>
       {/* <li>Learn React</li>
       <li>Learn TypeScript</li> */}
+
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        // <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
